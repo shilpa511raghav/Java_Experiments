7e432fe93dff9b7bd4e8cb1faa6eb3fe746212c8
shilpa511raghav <shilpa511raghav@gmail.com>
2019-10-03 22:53:39 +0530

    multithreading with example

--- 8)Thread_Demo/src/com/javaprograms/ThreadDemo.java
+++ 8)Thread_Demo/src/com/javaprograms/ThreadDemo.java
@@ -11,13 +11,30 @@ class MyThread1 extends Thread{
         }
     }
 }
+// Hook Thread
+class HookThread extends Thread{
+    public void run(){
+        System.out.println("Hook thread ");
+    }
+
+}
 
 public class ThreadDemo {
 
     public static void main(String[] args) {
         MyThread1 t1 = new MyThread1();
+        MyThread1 t2 = new MyThread1();
+        System.out.println(t1.getName());
+        System.out.println(t2.getName());
+        System.out.println(Thread.currentThread().getName());
         t1.start();  // thread created only when  start method is called
+        t2.start();
         System.out.println("Main Thread");
+
+
+        HookThread h = new HookThread();
+        Runtime r = Runtime.getRuntime(); 
+        r.addShutdownHook(h);
 
     }
 }
