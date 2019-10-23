993b3e0159e45760cb8a43f24f2cb9b8fa55e5c7
shilpa511raghav <shilpa511raghav@gmail.com>
2019-10-23 08:53:32 +0530

    updated using foreach loop

--- 11)_ArraysDemo/src/Student.java
+++ 11)_ArraysDemo/src/Student.java
@@ -8,12 +8,15 @@ public class Student {
             this.name= name;
         }
     public static void main(String[] args){
-        Student[] std; // declaration of Student object array
-        std = new Student[2]; // Instiatiation-> memory allocation
-        std[0] = new Student(1,"shilpa");
-        std[1] = new Student(2,"surbhi");
-        for (int i=0;i<std.length;i++){
-            System.out.println("student array"+std[i]);
-        }
+            Student[] std; // declaration of Student object array
+            std = new Student[2]; // Instiatiation-> memory allocation
+            std[0] = new Student(1,"shilpa");  // initialization =>assign value
+            std[1] = new Student(2,"surbhi");
+            for (int i=0;i<std.length;i++){
+                System.out.println("student array--"+i+"---"+std[i].name); // accessing
+            }
+            for(Student s: std){    // foreach loop
+                System.out.println(s.name);
+            }
     }
 }
