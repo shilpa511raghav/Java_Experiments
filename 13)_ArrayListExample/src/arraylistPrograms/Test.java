86db038edfa4a41d85c07e8f5c5e4dab69b42ed1
shilpa <shilpa511raghav@gmail.com>
2019-11-03 08:43:55 +0530

    collection framework -arrayList (heterogeneous data storage) example

---
+++ 13)_ArrayListExample/src/arraylistPrograms/Test.java
@@ -0,0 +1,17 @@
+package arraylistPrograms;
+
+import java.util.ArrayList;
+
+public class Test {
+    public static void main(String[] args){
+        ArrayList a1 = new ArrayList();
+//        heterogeneous insertion
+        a1.add(10); // integers insertion
+        a1.add(10.5); // double insertion
+        a1.add("test data"); // string insertion
+        a1.add("a"); // character insertion
+        a1.add(null); // null allowed
+        a1.add(10); // duplicate integer insertion
+        System.out.println(a1);
+    }
+}
